984ae7442c95fcd79d8fd0b964005b7d7b136562
mark.khomutsky <mark.khomutsky@buidl.ru>
2020-12-24 00:09:31 +0300

    deploy fix token

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,16 @@
 
 dotnet publish -c Release -o ./app DataLuna.Back/DataLuna.Back.csproj
 
-docker build -t cr.yandex/crpdcqv18l8nis2ebeu3/dtlback:1.0
+docker build . -t cr.yandex/crpdcqv18l8nis2ebeu3/dtlback:1.0
 
 docker login --username oauth --password AgAAAAARuFJyAATuwTeaZ9G5bETCkKI1ZXJ4n48 cr.yandex
 
 docker push cr.yandex/crpdcqv18l8nis2ebeu3/dtlback:1.0
 
 #remote host
-ssh dtluser@84.201.169.39 sudo docker login --username iam --password t1.9euelZqVxpOciZeUlo6JlprGlIublu3rnpWayJ3Hi5ySncnHnpqamsuejJvl8_d6HhsB-u8PQgku_t3z9zpNGAH67w9CCS7-.xz-UV_Pd_XYRGDCsGiiRVqnw14m0RnSX1oE4-z1XrJAJ5fmuMkgK7oP4M1BkEEUtbEjM0RDErs_tKBAJgq-4BA cr.yandex
+IAM_TOKEN=$(yc iam create-token)
+
+ssh dtluser@84.201.169.39 sudo docker login --username iam --password $IAM_TOKEN cr.yandex
 
 ssh dtluser@84.201.169.39 sudo docker pull cr.yandex/crpdcqv18l8nis2ebeu3/dtlback:1.0
 
