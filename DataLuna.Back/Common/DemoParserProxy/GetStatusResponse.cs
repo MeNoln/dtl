a99187f101a8b4094a3c4138d7d71ad361f0f8f7
mark.khomutsky <mark.khomutsky@buidl.ru>
2021-03-12 00:18:52 +0300

    fix

--- DataLuna.Back/Common/DemoParserProxy/GetStatusResponse.cs
+++ DataLuna.Back/Common/DemoParserProxy/GetStatusResponse.cs
@@ -4,9 +4,9 @@ namespace DataLuna.Back.Common.DemoParserProxy
 {
     public class GetStatusResponse
     {
-        public string Url { get; set; }
-        public bool FileCached { get; set; }
-        public string Status { get; set; }
-        public string PreparseCached { get; set; }
+        public string url { get; set; }
+        public bool fileCashed { get; set; }
+        public string status { get; set; }
+        public string preparseCashed { get; set; }
     }
 }
